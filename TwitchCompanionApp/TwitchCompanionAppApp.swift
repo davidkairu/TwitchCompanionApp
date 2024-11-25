253988eb7b88abae7a894391de6a95dbabed77dd
DAVID KAIRU <davidnjoroge560@gmail.com>
2024-11-25 03:46:09 -0500

    Initial Commit

---
+++ TwitchCompanionApp/TwitchCompanionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitchCompanionAppApp.swift
+//  TwitchCompanionApp
+//
+//  Created by DAVID N KAIRU on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitchCompanionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
